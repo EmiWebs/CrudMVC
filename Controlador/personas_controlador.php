d1984bd97d9cb25f2472b72edb028486c924c29f
Emilce <azucenaemi@gmail.com>
2021-01-31 23:24:56 +0100

    CRUD en MVC y paginación

--- Controlador/personas_controlador.php
+++ Controlador/personas_controlador.php
@@ -1,6 +1,6 @@
 <?php
     require_once ("Modelo/personas_modelo.php");
-    $persona = new Personas_modelo();
+    $persona = new Personas_modelo(); //Aca creo una instancia del objeto Personas_modelo
     $matrizPersonas = $persona->get_personas();
     require_once ("Vista/personas_vista.php");
 ?>
